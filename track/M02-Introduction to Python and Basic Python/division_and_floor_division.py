e37b4fad001f9e9acaf67831f10c8ee7c03e2d40
Jayasree <pjayasree543@gmail.com>
2026-08-04 16:37:24 +0530

    Division and Floor division

---
+++ track/M02-Introduction to Python and Basic Python/division_and_floor_division.py
@@ -0,0 +1,8 @@
+first_number = int(input())
+second_number = int(input())
+
+first_line = first_number / second_number
+second_line = first_number // second_number
+
+print(first_line)
+print(second_line)
