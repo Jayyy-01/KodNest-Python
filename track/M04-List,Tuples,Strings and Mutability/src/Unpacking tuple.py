16fe44c3f0068c49e5af5e117be7a7894589ed28
Jayasree <pjayasree543@gmail.com>
2026-08-07 15:38:34 +0530

    unpacking tuples

---
+++ track/M04-List,Tuples,Strings and Mutability/src/Unpacking tuple.py
@@ -0,0 +1,13 @@
+name = input()
+course = input()
+score = int(input())
+
+student_record = (name, course, score)
+
+student_record = name
+student_record = course
+student_record = score
+
+print(f"Name: {name}")
+print(f"Course: {course}")
+print(f"Score: {score}")
