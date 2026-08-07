ca9864f2defebf758b737891e672f69bf53edd8a
Jayasree <pjayasree543@gmail.com>
2026-08-07 16:08:01 +0530

    updating tuple without modifying

---
+++ track/M04-List,Tuples,Strings and Mutability/src/Updating tuple without modifying.py
@@ -0,0 +1,10 @@
+course_name = input()
+course_week = input()
+course_status = input()
+
+course_details = (course_name, course_week, course_status)
+updated_week = input()
+
+course_details = (course_name, updated_week, course_status)
+
+print(course_details)
