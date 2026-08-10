8a4d3858e1d7390769432be218d6cdee5d335020
Jayasree <pjayasree543@gmail.com>
2026-08-10 15:24:28 +0530

    shared list modification

---
+++ track/M04-List,Tuples,Strings and Mutability/src/Shared list modification.py
@@ -0,0 +1,18 @@
+original_scores = []
+
+for _ in range(3):
+    original_scores.append(int(input()))
+
+alias_scores = original_scores
+
+replacement_score = int(input())
+additional_score = int(input())
+
+alias_scores[0] = replacement_score
+alias_scores.append(additional_score)
+
+print(f"Original: {original_scores}")
+print(f"Alias: {alias_scores}")
+print(f"Shared Object: {original_scores == alias_scores}")
+
+#summary is if modification is done in alias_scores then the modification will be reflected in original_scores. 
