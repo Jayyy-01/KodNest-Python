95f712ac3c7941c1c9806b28efaf6df84848226e
Jayasree <pjayasree543@gmail.com>
2026-08-10 15:51:00 +0530

    slicing on tuple

---
+++ track/M04-List,Tuples,Strings and Mutability/src/Process student skill record.py
@@ -0,0 +1,23 @@
+skills = []
+
+#read and store 5 skills
+for i in range(5):
+    skills.append(input())
+
+#convert into tuple
+skills = tuple(skills)
+
+#create required slicing
+
+first_three = skills[0:3]
+last_two = skills[3:5]
+alternative = skills[0:5:2]
+reversed = skills[::-1]
+
+print(f"Skill Record: {skills}")
+print(f"First three Skills: {first_three}")
+print(f"Last two Skills: {last_two}")
+print(f"Alternate Skills: {alternative}")
+print(f"Reversed Skills: {reversed}")
+
+#Summary: we used slicing concept in tuples to create required slicing
