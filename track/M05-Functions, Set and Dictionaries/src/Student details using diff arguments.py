b60ef312b5479ff4445a4f592d03a2740d995880
Jayasree <pjayasree543@gmail.com>
2026-08-11 15:35:36 +0530

    different argument types

---
+++ track/M05-Functions, Set and Dictionaries/src/Student details using diff arguments.py
@@ -0,0 +1,8 @@
+def display_student(name, course, level):
+    print(f"{name} | {course} | {level}")
+
+display_student("Aarav", "Python", "Beginner")
+display_student(name="Meera",course="Java",level="Advanced")
+display_student("Kabir",course="SQL",level="Intermediate") 
+
+#here the values are passed as positional arguments and it is not necessary for them to be in the same order
