2c6355ac55adfae48ffbe6542b62323ccb7c6bce
Jayasree <pjayasree543@gmail.com>
2026-08-11 14:47:06 +0530

    calculating invoice

--- track/M05-Functions, Set and Dictionaries/src/Invoice total using function.py
+++ track/M05-Functions, Set and Dictionaries/src/Invoice total using function.py
@@ -1,16 +1,15 @@
 def invoice_total(price, quantity):
     total = price * quantity
     return total
-    passs
+    pass
 
+# price = int(input())
+# quantity = int(input())
 
-price = int(input())
-quantity = int(input())
-
-print(f"Total: {(invoice_total(price,quantity))}")
+print(f"Total: {(invoice_total(100, 2))}")
 
 #total = price * quantity is stored in invoice_total(price, quantity) and when it is called, it is returned and printed
 
 #print(f"Total: {(invoice_total(price,quantity))}") can be written as 
 # x = invoice_total(price,quantity)
-# print(f"Total: {x}")
+# print(f"Total: {x}") 
