4c7e3e9f450f5fea7c84287bf09552a1ce9dd601
Jayasree <pjayasree543@gmail.com>
2026-08-12 15:51:04 +0530

    book details using class

---
+++ track/M06-Classes and Object/src/Build a book class.py
@@ -0,0 +1,19 @@
+class Book:
+    def __init__(self, title, author, price):
+        self.title = title
+        self.author = author
+        self.price = price
+
+title = input()
+author = input()
+price = int(input())
+
+book = Book(title, author, price)
+print("BOOK DETAILS")
+print(f"Book Title: {book.title}")
+print(f"Author: {book.author}")
+print(f"Price: {book.price}")
+
+#define a class book with __init__ method that accepts title, author, and price
+#read the values from user input and create an instance of the book class
+#print all details of the book in the specified format
