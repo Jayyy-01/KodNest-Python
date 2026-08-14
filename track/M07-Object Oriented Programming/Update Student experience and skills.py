d3780012cbfb32235eaf7101506cd7cd90a684ca
Jayasree <pjayasree543@gmail.com>
2026-08-14 16:50:59 +0530

    updating experience and adding skill in student profile using different methods

--- track/M07-Object Oriented Programming/Update Student experience and skills.py
+++ track/M07-Object Oriented Programming/Update Student experience and skills.py
@@ -7,7 +7,7 @@ class StudentProfile:
     def update_experience(self,new_experience):
         self.experience = new_experience
     
-    def update_skills(self,new_skill):
+    def add_skill(self,new_skill):
         self.skills.append(new_skill)
 
 name = input().strip()
@@ -21,7 +21,7 @@ s = StudentProfile(name,experience,skills)
 #update the studen's experience
 s.update_experience(new_experince) 
 #update the student's skill
-s.skills.append(new_skill)
+s.add_skill(new_skill)
 
 print(f"Student Name: {s.name}")
 print(f"Updated Experience: {s.experience}")
