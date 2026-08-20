1e35df409277100c5eb72c749287faedf8c599c0
Jayasree <pjayasree543@gmail.com>
2026-08-20 16:10:08 +0530

    accessing private data using decorators

---
+++ track/M07-Object Oriented Programming/accessing private variables through attributes.py
@@ -0,0 +1,21 @@
+class Student:
+    def __init__(self,roll,name):
+        self.__roll = roll
+        self.__name = name
+    
+    @property
+    def roll(self):
+        return self.__roll
+    
+    @property
+    def name(self):
+        return self.__name 
+
+s1 = Student(12,"jay") 
+print(s1.roll)
+print(s1.name) 
+
+
+#here we are accessing data of roll and name using direct attributes(through @property, which is same syntax as getter)
+#usually private attributes are not accessed directly, but through getter and setter methods it is possible
+#here we are accessing using @property which is a decorator, using this we can access the private variables directly
