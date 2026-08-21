72d7f5df3a671cab89dd72550ba2feb21464c913
Jayasree <pjayasree543@gmail.com>
2026-08-21 16:42:24 +0530

    shadowing demo

---
+++ track/M07-Object Oriented Programming/shadowing demo.py
@@ -0,0 +1,9 @@
+class StudentProfile:
+    profile_count = 0   # class variable
+
+    def __init__(self, name):
+        self.name = name
+        self.profile_count += 1   # BUG: this shadows, doesn't update the class var!
+
+
+#solution: change the 7th line as StudentProfile.profile_count += 1
