8502ce5a19492c7255fdfe8201efd5b2d283a08d
Jayasree <pjayasree543@gmail.com>
2026-08-31 15:58:20 +0530

    hierarchial inheritance

---
+++ track/M08-Inheritance/src/create fresher and experince students.py
@@ -0,0 +1,24 @@
+class StudentProfile:
+    def __init__(self, name):
+        self.name = name
+
+    def display_profile(self, category):
+        print(f"{category}: {self.name}")
+
+
+class FresherStudent(StudentProfile):
+    pass
+
+class ExperiencedStudent(StudentProfile):
+    pass
+
+
+fresher_name = input().strip()
+experienced_name = input().strip()
+
+# Create both objects and display their profiles
+fresher = FresherStudent(fresher_name)
+experienced = ExperiencedStudent(experienced_name)
+
+fresher.display_profile("Fresher Student")
+experienced.display_profile("Experienced Student")
